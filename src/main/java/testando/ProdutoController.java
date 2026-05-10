0a16d39e352f9c76702a17ac5f01d7acb8f43cf1
evertonpires1234 <everton.pires@gmail.com>
2026-05-10 12:17:17 -0300

    teste

---
+++ src/main/java/testando/ProdutoController.java
@@ -0,0 +1,38 @@
+package testando;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private List<Produto> produtos = new ArrayList<>();
+
+    // GET
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtos;
+    }
+
+    // POST
+    @PostMapping
+    public String adicionarProduto(@RequestBody Produto produto) {
+        produtos.add(produto);
+        return "Produto adicionado com sucesso!";
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String removerProduto(@PathVariable Long id) {
+
+        produtos.removeIf(produto -> produto.getId().equals(id));
+
+        return "Produto removido com sucesso!";
+    }
+
+}
---
+++ src/main/java/testando/SpringWebApiApplication.java
@@ -0,0 +1,13 @@
+package testando;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApiApplication.class, args);
+    }
+}
